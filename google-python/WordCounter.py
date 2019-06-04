3b08e0a427eacec3493c5b2dae271e8ca6e13f69
Sky <sky.willis@outlook.com>
2019-06-04 13:16:41 +0100

    Fixed bugs, works as Google exercise works

--- google-python/WordCounter.py
+++ google-python/WordCounter.py
@@ -3,7 +3,7 @@ import sys
 dictOfWords = {}
 
 def readFile(filename):
-    f = open(filename, 'r')
+    f = open(filename, 'rU')
     for line in f:
         words = line.split()
         for word in words:
@@ -18,30 +18,34 @@ def readFile(filename):
 def print_words(filename):
     redFile = sorted(readFile(filename))
     for key in redFile:
-        print( key + " " + str(dict[key]))
+        print( key + " " + str(dictOfWords[key]))
 
 def print_top(filename):
     redFile = sorted(readFile(filename),key=secondInTuple, reverse = True)
     for key in redFile[:20]:
-        print(key + " " + str(dict[key]))
+        print(key + " " + str(dictOfWords[key]))
 
 def secondInTuple(tuple):
-    return dict[tuple]
+    return dictOfWords[tuple]
+
 
 def main():
-   if ( len(sys.argv) != 3):
-       print('Please Enter:\n\nWordCounter.py <--count / --topcount> <filename>')
-   sys.exit(1)
+    if ( len(sys.argv) != 3):
+       print('\n\nPlease Enter:\nWordCounter.py <--count / --topcount> <filename>')
+       sys.exit(1)
 
-   countType = sys.argv[1]
-   filename = sys.argv[2]
-   if option == '--count':
-     print_words(filename)
-   elif option == '--topcount':
-     print_top(filename)
-   else:
-     print(countType + " not a valid option. Please choose again.")
-     sys.exit(1)
+    countType = sys.argv[1]
+    filename = sys.argv[2]
+    print(countType, filename)
+    if countType == '--count':
+         print_words(filename)
+    elif countType == '--topcount':
+         print_top(filename)
+    else:
+         print(countType + " not a valid option. Please choose again.")
+         sys.exit(1)
+
+
 
 if __name__ == '__main__':
-  main()
+    main()
