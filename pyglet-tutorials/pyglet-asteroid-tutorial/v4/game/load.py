1c99c95c424ee98f6f96ce8540f4f81a6cfd68cd
Sky <sky.willis@outlook.com>
2019-06-21 12:48:44 +0100

    copied v3 over to v4 folder

---
+++ pyglet-tutorials/pyglet-asteroid-tutorial/v4/game/load.py
@@ -0,0 +1,36 @@
+import pyglet
+import random
+from . import resources, physicalobject, util
+import math
+
+asteroid_list = []
+
+def player_lives(num_icons, batch=None):
+    player_lives = []
+    for icon in range(num_icons):
+        new_sprite = pyglet.sprite.Sprite(img=resources.player_image,
+                                          x=784-icon*30,
+                                          y=585,
+                                          batch = batch)
+        new_sprite.scale = 0.5
+        player_lives.append(new_sprite)
+    return player_lives
+
+def asteroids(num_asteroids, player_position, batch=None):
+
+    for asteroid in range(num_asteroids):
+
+        asteorid_x, asteroid_y = player_position
+
+        while(util.distance((asteorid_x,asteroid_y), player_position) < 100):
+            asteroid_x = random.randint(0,800)
+            asteroid_y = random.randint(0,600)
+        new_asteroid = physicalobject.PhysicalObject(img=resources.asteroid_image,
+                                            x=asteroid_x, y=asteroid_y,
+                                            batch = batch)
+        new_asteroid.rotation = random.randint(0,360)
+        new_asteroid.velocity_x = random.random()*40
+        new_asteroid.velocity_y = random.random()*40
+        asteroid_list.append(new_asteroid)
+
+    return asteroid_list
---
+++ pyglet-tutorials/pyglet-asteroid-tutorial/v4/game/resources.py
@@ -0,0 +1,21 @@
+import pyglet
+
+pyglet.resource.path = ["C:\\Users\\skyli\\Documents\\Uni\\SLICC\\SLICC\\pyglet-tutorials\\pyglet-asteroid-tutorial\\resources\\"]
+pyglet.resource.reindex()
+
+player_image = pyglet.resource.image("player.png")
+bullet_image = pyglet.resource.image("bullet.png")
+asteroid_image = pyglet.resource.image("asteroid.png")
+engine_image = pyglet.resource.image("flame.png")
+
+engine_image.anchor_x - engine_image.width * 1.5
+engine_image.anchor_y = engine_image.height / 2
+
+
+def center_image(image):
+    image.anchor_x = image.width / 2
+    image.anchor_y = image.height / 2
+
+center_image(player_image)
+center_image(bullet_image)
+center_image(asteroid_image)
